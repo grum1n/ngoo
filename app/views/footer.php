b27d69e901c8bf94a313e5c799864191da7e8045
grum1n <grumin.el.pastas@gmail.com>
2021-08-24 16:51:08 +0300

    Added footer navigation

--- app/views/footer.php
+++ app/views/footer.php
@@ -5,6 +5,46 @@
            <button class="c2"><span>DONATE NOW</span></button>
        </div>
     </div>
+    <div>
+        <div class="bottom-nav">
+            <div class="container bottom-box-col-i">
+                <ul class="col-1 bottom-nav-boxes">
+                    <li>
+                        <a href="index.php"><img src="app/images/logo-bottom.png" alt="Logo Ngoo"></a> 
+                    </li>
+                    <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur sunt in culpa qui officia deserunt mollit anim qui officia deserunt mollit anim id est laborum.</li>
+                </ul>
+                <ul class="col-2 bottom-nav-boxes heading-orange-line">
+                    <li>WHO WE ARE</li>
+                    <li><a href="index.php"><i class="chevron-icon-size fas fa-chevron-right"></i> Home</a></li>
+                    <li><a href=""><i class="chevron-icon-size fas fa-chevron-right"></i> About</a></li>
+                    <li><a href=""><i class="chevron-icon-size fas fa-chevron-right"></i> Couses</a></li>
+                    <li><a href=""><i class="chevron-icon-size fas fa-chevron-right"></i> Events</a></li>
+                    <li><a href=""><i class="chevron-icon-size fas fa-chevron-right"></i> Contact</a></li>
+                </ul>
+                <ul class="col-3 bottom-nav-boxes heading-orange-line">
+                    <li>WHERE WE WORK</li>
+                    <li><a class="white-icon-align" href="https://www.google.com/maps/place/Vilnius+Coding+School+-+programavimo+kursai/@54.7099117,25.2901282,17z/data=!3m1!4b1!4m5!3m4!1s0x46dd96b1c1c3bc9f:0xb4eca31b162c5579!8m2!3d54.7099406!4d25.2921824" target="_blank">
+                        <span ><i class="white-icon-padding fas fa-map"></i></span>
+                        <span>Naujoji Akmenė. Lithuania</span></a>
+                    </li>
+                    <li><a href="tel:+37064766888"><i class="white-icon-padding fas fa-phone-alt"></i> (+370) 64766888</a></li>
+                    <li><a href="mailto:info@yoursite.com"><i class="white-icon-padding fas fa-envelope"></i> info@yoursite.com</a></li>
+                    <li><i class="white-icon-padding fas fa-clock"></i> Mon - Sat 09:00 - 17:00</li>
+                </ul>
+                <ul class="col-4 bottom-nav-boxes heading-orange-line">
+                    <li>FOLLOW US</li>
+                    <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </li>
+                    <li class="social-icon-padding">
+                        <a href="https://www.facebook.com/" target="_blank" title="www.facebook.com"><i class="fab fa-facebook"></i></a>
+                        <a href="https://twitter.com/" target="_blank" title="www.twitter.com"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.instagram.com/" target="_blank" title="www.instagram.com"><i class="fab fa-instagram"></i></a> 
+                        <a href="https://www.youtube.com/"  target="_blank" title="www.youtube.com"><i class="fab fa-youtube"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
     <div class="gray-footer-bg">
         <div class="container">
             Copyright <?php echo date('Y'); ?> © NGOO Figma Template. Designed by Rometheme.
