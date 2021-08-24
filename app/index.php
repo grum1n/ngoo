80e73e7e7e876415358a05fbb35221c32f34a778
grum1n <grumin.el.pastas@gmail.com>
2021-08-24 11:56:24 +0300

    Added meta info in to head section

--- app/index.php
+++ app/index.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Ngoo web design">
+    <meta name="keywords" content="HTML, html, CSS, css, JavaScript, javascript, PHP, php , SQL, sql, full-stack kursai, ngoo dizainas, Ngoo dizainas">
+    <meta name="author" content="grumin">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ngoo | index</title>
     <link rel="stylesheet" href="app/css/style.css">
