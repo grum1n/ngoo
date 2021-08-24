c14a19b1e03fc2ea620950859f658f21f59e90d9
grum1n <grumin.el.pastas@gmail.com>
2021-08-24 20:34:46 +0300

    Added section we-need with file

---
+++ app/views/inc/we-need-help.php
@@ -0,0 +1,55 @@
+<section class="white-container we-need">
+    <div class="container">
+        <div class="section-heading">
+            <h1>We <span class="text-orange">Need</span> Your Help</h1>
+            <p class="text-small">Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="help flex-container">
+            <a href="">
+            <div class="col-1 need-help-box">
+                <img src="app/images/education.jpg" alt="Education">
+                <div>
+                    <h6>EDUCATION FOR SYRIAN CHILD</h6>
+                    <p  class="text-small">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent.</p>
+                    <div class="price-table">
+                        <span class="text-orange">$2,580</span>
+                        <span>to go</span>
+                        <span>80%</span>
+                    </div>
+                </div>
+            </div>
+            </a>
+            <a href="">
+            <div  class="col-2 need-help-box">
+                <img src="app/images/home-children.jpg" alt="home children">
+                <div>
+                    <h6>HOME FOR KAMPAR'S CHILD</h6>
+                    <p  class="text-small">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent.</p>
+                    <div class="price-table">
+                        <span class="text-orange">$2,580</span>
+                        <span>to go</span>
+                        <span>80%</span>
+                    </div>
+                </div> 
+            </div>
+            </a>
+            <a href="">
+            <div class="col-3 need-help-box">
+                <img src="app/images/water.jpg" alt="water">
+                <div>
+                    <h6>CLEAN WATER FOR SOUTH SUDAN'S</h6>
+                    <p  class="text-small">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent.</p>
+                    <div class="price-table">
+                        <span class="text-orange">$2,580</span>
+                        <span>to go</span>
+                        <span>80%</span>
+                    </div>
+                </div> 
+            </div>
+            </a>
+        </div>
+        <div class="we-need-button">
+            <a href="">SEE ALL CAUSE</a>
+        </div>
+    </div>
+</section>
