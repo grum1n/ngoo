7c4b6057a5da319273aa8150de80bb739bf34b3e
grum1n <grumin.el.pastas@gmail.com>
2021-08-24 17:31:12 +0300

    Added jQuery file

--- app/views/main.php
+++ app/views/main.php
@@ -1,3 +1,24 @@
 <main>
     text
+    <p>tt</p>
+    <p>tt</p>
+
+    <p>tt</p>
+    <p>tt</p><p>tt</p>
+    <p>tt</p>
+
+    <p>tt</p>
+    <p>tt</p>
+    <p>tt</p>
+    variant_div
+    <p>tt</p><p>tt</p>
+    <p>tt</p><p>tt</p>
+    <p>tt</p>
+    <p>tt</p>
+    <p>tt</p>
+    v
+    <p>tt</p>
+    <p>tt</p>
+    <p>tt</p>
+    
 </main>
