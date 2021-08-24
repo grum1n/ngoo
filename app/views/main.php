8888dbd6ce5fdf1c8363c1922add906916666d67
grum1n <grumin.el.pastas@gmail.com>
2021-08-24 19:07:11 +0300

    Added hero section

--- app/views/main.php
+++ app/views/main.php
@@ -1,24 +1,43 @@
 <main>
-    text
-    <p>tt</p>
-    <p>tt</p>
-
-    <p>tt</p>
-    <p>tt</p><p>tt</p>
-    <p>tt</p>
-
-    <p>tt</p>
-    <p>tt</p>
-    <p>tt</p>
-    variant_div
-    <p>tt</p><p>tt</p>
-    <p>tt</p><p>tt</p>
-    <p>tt</p>
-    <p>tt</p>
-    <p>tt</p>
-    v
-    <p>tt</p>
-    <p>tt</p>
-    <p>tt</p>
+<!--  HERO  -->
+<section class="ngoo-slideshow-container">
+    <div class="ngoo-mySlides ngoo-fade">
+      <div class="ngoo-numbertext">1 / 3</div>
+      <img class="hero-slider" src="app/images/pexels-568021.jpg" alt="Pexels kat jayne">
+      <div class="slider-content-text">
+          <h1><span class="text-orange">#EndViolence</span> For Every Woman</h1>
+          <p class="ngoo-fade-small-text">Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <button class="a2"><span>DONATE NOW</span></button>
+      </div>
+      <h3 class="ngoo-text">End Violence For Every Woman</h3>
+    </div>
+    <div class="ngoo-mySlides ngoo-fade">
+      <div class="ngoo-numbertext">2 / 3</div>
+      <img class="hero-slider" src="app/images/pexels-333142.jpg" alt="Pexels tijana bosnjakov">
+      <div class="slider-content-text">
+          <h1><span class="text-orange">#EndViolence</span> For Every Woman</h1>
+          <p class="ngoo-fade-small-text">Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <button class="a2"><span>DONATE NOW</span></button>
+      </div>
+      <h3 class="ngoo-text">Stop violence</h3>
+    </div>
+    <div class="ngoo-mySlides ngoo-fade">
+      <div class="ngoo-numbertext">3 / 3</div>
+      <img class="hero-slider" src="app/images/pexels-5723318.jpg" alt="Pexels anete lusina">
+      <div class="slider-content-text">
+          <h1><span class="text-orange">#EndViolence</span> For Every Woman</h1>
+          <p class="ngoo-fade-small-text">Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <button class="a2"><span>DONATE NOW</span></button>
+      </div>
+      <h3 class="ngoo-text">Silence allows violence</h3>
+    </div>
+    <a class="ngoo-prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="ngoo-next" onclick="plusSlides(1)">&#10095;</a>
+    <div class="ngoo-dot-center">
+      <span class="ngoo-dot" onclick="currentSlide(1)"></span> 
+      <span class="ngoo-dot" onclick="currentSlide(2)"></span> 
+      <span class="ngoo-dot" onclick="currentSlide(3)"></span> 
+    </div>
+</section>
     
 </main>
