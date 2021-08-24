8a335a019ee46f2a425f31502ec2447e99655cc1
grum1n <grumin.el.pastas@gmail.com>
2021-08-24 16:35:31 +0300

    added in to footer gray-footer-bg

--- app/views/footer.php
+++ app/views/footer.php
@@ -5,5 +5,10 @@
            <button class="c2"><span>DONATE NOW</span></button>
        </div>
     </div>
+    <div class="gray-footer-bg">
+        <div class="container">
+            Copyright <?php echo date('Y'); ?> © NGOO Figma Template. Designed by Rometheme.
+        </div>
+    </div>
 </footer>
 <script src="app/js/custom.js"></script>
