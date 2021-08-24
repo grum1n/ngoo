1324c45bed3e4984c5ddf97d659315237df0462b
grum1n <grumin.el.pastas@gmail.com>
2021-08-24 16:13:34 +0300

    added footer > orange line

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,4 +1,9 @@
 <footer>
-
+    <div class="orange-footer-bg">
+       <div class="container">
+           <h5>Join your hand with us for a better life and beautiful future.</h5>
+           <button class="c2"><span>DONATE NOW</span></button>
+       </div>
+    </div>
 </footer>
 <script src="app/js/custom.js"></script>
