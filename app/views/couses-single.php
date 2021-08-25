2b242571cdf6b989ac092b961bc964d759aa03a4
grum1n <grumin.el.pastas@gmail.com>
2021-08-25 13:50:49 +0300

    hide img on mobile

---
+++ app/views/couses-single.php
@@ -0,0 +1,10 @@
+<section class="small-hero">
+    <div class="container">
+        <h1>Couses Single</h1>
+        <p>Home / Couses single</p>
+    </div>
+</section>
+<?php
+//WE NEED YOUR HELP Single
+include('app/views/inc/couses-we-need-help-single.php');
+?>
---
+++ app/views/inc/couses-we-need-help-single.php
@@ -0,0 +1,35 @@
+<section class="white-container">
+    <div class="container">
+        <div class="urgent-single-grid">
+            <div class="col-1">
+                
+            </div>
+            <div class="col-2">
+                <h1 class="head-underline"><span class="text-orange">Urgent</span> Cause</h1>
+                <h1><span class="text-orange">#School</span> for South Kampar's Students</h1>
+                <h3>Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent.</h3>
+                <p>Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis iste natus error sit. Taccusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium.</p>
+                <h2>The Program We <span  class="text-orange">Do</span> ?</h2>
+                <ol>
+                    <li>Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</li>
+                    <li>Accusantium dolore mque laudantium, totam rem aperiam</li>
+                    <li>Eaque ipsa quae ab illo invent.</li>
+                    <li> Sed ut perspiciatis unde omnis iste natus error sit.</li>
+                </ol>
+                <div class="downl-brochure flex-container">
+                <div>
+                    <h6>Download Brochure</h6>   
+                    <p>Click here to download .PDF</p>
+                </div>
+                 <a href=""><i class="fas fa-file-pdf"></i></a>
+            </div>
+            </div>
+            <div class="col-3">
+                <img src="app/images/lagos-techie.jpg" alt="">
+                <h6 class="orange-line orange-long-line">Donated <span class="text-gray-3">80%</span></h6>
+                <h6>$18,500 <span class="text-gray-3">Raised</span> of  $25,000 <span class="text-gray-3">Goal</span> </h6>
+            </div>
+            
+</div>
+    </div>
+</section>
