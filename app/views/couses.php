0aa19253b01e907c6ea6da6e5276590840eeaaa8
grum1n <grumin.el.pastas@gmail.com>
2021-08-25 12:43:58 +0300

    Added content in to couses page

---
+++ app/views/couses.php
@@ -0,0 +1,10 @@
+<section class="small-hero">
+    <div class="container">
+        <h1>Couses</h1>
+        <p>Home / Couses</p>
+    </div>
+</section>
+<?php
+//WE NEED YOUR HELP
+include('app/views/inc/couses-we-need-help.php');
+?>
---
+++ app/views/inc/couses-we-need-help.php
@@ -0,0 +1,92 @@
+<section class="white-container">
+    <div class="container">
+        <div class="help flex-container">
+            <a href="pages.php?page=couses-single">
+            <div class="col-1 need-help-box">
+                <img src="app/images/education.jpg" alt="Education">
+                <div>
+                    <h6>EDUCATION FOR SYRIAN CHILD</h6>
+                    <p  class="text-small">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent.</p>
+                    <div class="price-table">
+                        <span class="text-orange">$2,580</span>
+                        <span>to go</span>
+                        <span>80%</span>
+                    </div>
+                </div>
+            </div>
+            </a>
+            <a href="pages.php?page=couses-single">
+            <div  class="col-2 need-help-box">
+                <img src="app/images/home-children.jpg" alt="home children">
+                <div>
+                    <h6>HOME FOR KAMPAR'S CHILD</h6>
+                    <p  class="text-small">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent.</p>
+                    <div class="price-table">
+                        <span class="text-orange">$2,580</span>
+                        <span>to go</span>
+                        <span>80%</span>
+                    </div>
+                </div> 
+            </div>
+            </a>
+            <a href="pages.php?page=couses-single">
+            <div class="col-3 need-help-box">
+                <img src="app/images/water.jpg" alt="water">
+                <div>
+                    <h6>CLEAN WATER FOR SOUTH SUDAN'S</h6>
+                    <p  class="text-small">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent.</p>
+                    <div class="price-table">
+                        <span class="text-orange">$2,580</span>
+                        <span>to go</span>
+                        <span>80%</span>
+                    </div>
+                </div> 
+            </div>
+            </a>
+        </div>
+        <div class="help flex-container">
+            <a href="pages.php?page=couses-single">
+            <div class="col-4 need-help-box">
+                <img src="app/images/education.jpg" alt="Education">
+                <div>
+                    <h6>EDUCATION FOR SYRIAN CHILD</h6>
+                    <p  class="text-small">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent.</p>
+                    <div class="price-table">
+                        <span class="text-orange">$2,580</span>
+                        <span>to go</span>
+                        <span>80%</span>
+                    </div>
+                </div>
+            </div>
+            </a>
+            <a href="pages.php?page=couses-single">
+            <div  class="col-5 need-help-box">
+                <img src="app/images/home-children.jpg" alt="home children">
+                <div>
+                    <h6>HOME FOR KAMPAR'S CHILD</h6>
+                    <p  class="text-small">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent.</p>
+                    <div class="price-table">
+                        <span class="text-orange">$2,580</span>
+                        <span>to go</span>
+                        <span>80%</span>
+                    </div>
+                </div> 
+            </div>
+            </a>
+            <a href="pages.php?page=couses-single">
+            <div class="col-6 need-help-box">
+                <img src="app/images/water.jpg" alt="water">
+                <div>
+                    <h6>CLEAN WATER FOR SOUTH SUDAN'S</h6>
+                    <p  class="text-small">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent.</p>
+                    <div class="price-table">
+                        <span class="text-orange">$2,580</span>
+                        <span>to go</span>
+                        <span>80%</span>
+                    </div>
+                </div> 
+            </div>
+            </a>
+        </div>
+    </div>
+</section>
