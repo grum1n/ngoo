c80bf341a242061fb6860c23a09acdfde36b20ac
grum1n <grumin.el.pastas@gmail.com>
2021-08-26 18:57:08 +0300

    added gallery page content

--- app/views/inc/gallery-content.php
+++ app/views/inc/gallery-content.php
@@ -10,31 +10,41 @@
                 <a data-fancybox="gallery1" href="app/images/delfinai.jpg">
 		            <img src="app/images/delfinai-m.jpg" alt="Description">
 		        </a>
-
             </div>
             <div class="col-3 foto-box">
                <a data-fancybox="gallery1" href="app/images/kalnai.jpg">
 		            <img src="app/images/kalnai-m.jpg" alt="Description">
 		        </a>
-
             </div>
             <div class="col-4 foto-box">
                 <a data-fancybox="gallery1" href="app/images/miskas.jpg">
 		            <img src="app/images/miskas-m.jpg" alt="Description">
 		        </a>
-
             </div>
             <div class="col-5 foto-box">
                <a data-fancybox="gallery1" href="app/images/upe.jpg">
 		            <img src="app/images/upe-m.jpg" alt="Description">
 		        </a>
-
             </div>
             <div class="col-6 foto-box">
                 <a data-fancybox="gallery1" href="app/images/pasvaiste.jpg">
 		            <img src="app/images/pasvaiste-m.jpg" alt="Pašvaistė">
 		        </a>
-
+            </div>
+            <div class="col-7 foto-box">
+                <a data-fancybox="gallery1" href="app/images/galerija-mid.jpg">
+		            <img src="app/images/galerija-small.jpg" alt="Description">
+		        </a>
+            </div>
+            <div class="col-8 foto-box">
+                <a data-fancybox="gallery1" href="app/images/delfinai.jpg">
+		            <img src="app/images/delfinai-m.jpg" alt="Description">
+		        </a>
+            </div>
+            <div class="col-9 foto-box">
+               <a data-fancybox="gallery1" href="app/images/kalnai.jpg">
+		            <img src="app/images/kalnai-m.jpg" alt="Description">
+		        </a>
             </div>
         </div>
         
