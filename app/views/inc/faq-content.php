f1489cb1751cb63a1509ca125b1536a15aaebe59
grum1n <grumin.el.pastas@gmail.com>
2021-08-26 10:12:20 +0300

    Changed responsive layout
     for how to help us

---
+++ app/views/inc/faq-content.php
@@ -0,0 +1,36 @@
+<section class="white-container">
+    <div class="container faq-grid">
+        <div class="col-1 how-help-right">
+            <h1>How To <span class="text-orange">Help</span> Us</h1>
+            <p>Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent.</p>
+            <ul>
+                <li><i class="how-help-icon fas fa-gift"></i><h5>Send Donation</h5> 
+                    <p>Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore.</p>
+                </li>
+                <li><i class="how-help-icon fas fa-user-tie"></i><h5>Become Volunteer</h5>
+                    <p>Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore.</p> 
+                </li>
+                <li><i class="how-help-icon fas fa-bullhorn"></i><h5>Share Media</h5>
+                    <p>Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore.</p>
+                </li>
+            </ul>
+        </div>
+        <div class="col-2">
+            <ul>
+                <li><i class="orange-box-icon fas fa-bullhorn"></i></li>
+                <li><h6>Become a Volunteer</h6></li>
+                <li>We need you now for world</li>
+                <li>Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</li>
+                <li><button class="c2">JOIN US NOW</button></li>
+            </ul>
+            <div class="downl-brochure flex-container">
+                <div>
+                    <h6>Download Brochure</h6>   
+                    <p>Click here to download .PDF</p>
+                </div>
+                 <a href=""><i class="fas fa-file-pdf"></i></a>
+            </div>
+        </div>
+
+    </div>
+</section>
