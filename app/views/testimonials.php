8dd7fae784870edc5fbbd93d436070c190da098d
grum1n <grumin.el.pastas@gmail.com>
2021-08-26 19:11:57 +0300

    added new page testimonials

---
+++ app/views/testimonials.php
@@ -0,0 +1,10 @@
+<section class="small-hero">
+    <div class="container">
+        <h1>Testimonials</h1>
+        <p>Home / Testimonials</p>
+    </div>
+</section>
+<?php
+//events content
+include('app/views/inc/testimonials-content.php');
+?>
