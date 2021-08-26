8eca11e2b78792ecf830f6b95f720fde08d255fc
grum1n <grumin.el.pastas@gmail.com>
2021-08-26 12:15:15 +0300

    added profile fotos

--- app/views/team.php
+++ app/views/team.php
@@ -7,91 +7,91 @@
 <section  class="white-container">
     <div class="container team-profile">
         <ul class="flex-col-1">
-            <li><img src="app/images/co-founder.jpg" alt="Co founder"></li>
+            <li><img src="app/images/c-founder2.jpg" alt="Co founder 2"></li>
             <li><h6>Oliver Doel</h6></li>
             <li>CEO & Founder</li>
             <li class="flex-around">
-                <i class="fab fa-facebook"></i>
-                <i class="fab fa-twitter"></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-youtube"></i>
+                <a href="https://www.facebook.com/" target="_blank" title="www.facebook.com"><i class="fab fa-facebook"></i></a>
+                <a href="https://twitter.com/" target="_blank" title="www.twitter.com"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/" target="_blank" title="www.instagram.com"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.youtube.com/" target="_blank" title="www.youtube.com"><i class="fab fa-youtube"></i></a>
             </li>
         </ul>
         <ul class="flex-col-1">
             <li><img src="app/images/co-founder.jpg" alt="Co founder"></li>
-            <li><h6>Oliver Doel</h6></li>
+            <li><h6>Ella Doel</h6></li>
             <li>CEO & Founder</li>
             <li class="flex-around">
-                <i class="fab fa-facebook"></i>
-                <i class="fab fa-twitter"></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-youtube"></i>
+                <a href="https://www.facebook.com/" target="_blank" title="www.facebook.com"><i class="fab fa-facebook"></i></a>
+                <a href="https://twitter.com/" target="_blank" title="www.twitter.com"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/" target="_blank" title="www.instagram.com"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.youtube.com/" target="_blank" title="www.youtube.com"><i class="fab fa-youtube"></i></a>
+            </li>
+        </ul>
+        <ul class="flex-col-1">
+            <li><img src="app/images/benn.jpg" alt="Seo Benn"></li>
+            <li><h6>Benn Doel</h6></li>
+            <li>SEO</li>
+            <li class="flex-around">
+                <a href="https://www.facebook.com/" target="_blank" title="www.facebook.com"><i class="fab fa-facebook"></i></a>
+                <a href="https://twitter.com/" target="_blank" title="www.twitter.com"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/" target="_blank" title="www.instagram.com"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.youtube.com/" target="_blank" title="www.youtube.com"><i class="fab fa-youtube"></i></a>
+            </li>
+        </ul>
+        <ul class="flex-col-1">
+            <li><img src="app/images/lisa.jpg" alt="SEO Lisa"></li>
+            <li><h6>Lisa Doel</h6></li>
+            <li>SEO</li>
+            <li class="flex-around">
+                <a href="https://www.facebook.com/" target="_blank" title="www.facebook.com"><i class="fab fa-facebook"></i></a>
+                <a href="https://twitter.com/" target="_blank" title="www.twitter.com"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/" target="_blank" title="www.instagram.com"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.youtube.com/" target="_blank" title="www.youtube.com"><i class="fab fa-youtube"></i></a>
             </li>
         </ul>
         <ul class="flex-col-1">
             <li><img src="app/images/co-founder.jpg" alt="Co founder"></li>
-            <li><h6>Oliver Doel</h6></li>
+            <li><h6>Ella Doel</h6></li>
             <li>CEO & Founder</li>
             <li class="flex-around">
-                <i class="fab fa-facebook"></i>
-                <i class="fab fa-twitter"></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-youtube"></i>
+                <a href="https://www.facebook.com/" target="_blank" title="www.facebook.com"><i class="fab fa-facebook"></i></a>
+                <a href="https://twitter.com/" target="_blank" title="www.twitter.com"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/" target="_blank" title="www.instagram.com"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.youtube.com/" target="_blank" title="www.youtube.com"><i class="fab fa-youtube"></i></a>
             </li>
         </ul>
         <ul class="flex-col-1">
             <li><img src="app/images/co-founder.jpg" alt="Co founder"></li>
-            <li><h6>Oliver Doel</h6></li>
+            <li><h6>Ella Doel</h6></li>
             <li>CEO & Founder</li>
             <li class="flex-around">
-                <i class="fab fa-facebook"></i>
-                <i class="fab fa-twitter"></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-youtube"></i>
+                <a href="https://www.facebook.com/" target="_blank" title="www.facebook.com"><i class="fab fa-facebook"></i></a>
+                <a href="https://twitter.com/" target="_blank" title="www.twitter.com"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/" target="_blank" title="www.instagram.com"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.youtube.com/" target="_blank" title="www.youtube.com"><i class="fab fa-youtube"></i></a>
             </li>
         </ul>
         <ul class="flex-col-1">
             <li><img src="app/images/co-founder.jpg" alt="Co founder"></li>
-            <li><h6>Oliver Doel</h6></li>
+            <li><h6>Ella Doel</h6></li>
             <li>CEO & Founder</li>
             <li class="flex-around">
-                <i class="fab fa-facebook"></i>
-                <i class="fab fa-twitter"></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-youtube"></i>
+                <a href="https://www.facebook.com/" target="_blank" title="www.facebook.com"><i class="fab fa-facebook"></i></a>
+                <a href="https://twitter.com/" target="_blank" title="www.twitter.com"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/" target="_blank" title="www.instagram.com"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.youtube.com/" target="_blank" title="www.youtube.com"><i class="fab fa-youtube"></i></a>
             </li>
         </ul>
         <ul class="flex-col-1">
             <li><img src="app/images/co-founder.jpg" alt="Co founder"></li>
-            <li><h6>Oliver Doel</h6></li>
+            <li><h6>Ella Doel</h6></li>
             <li>CEO & Founder</li>
             <li class="flex-around">
-                <i class="fab fa-facebook"></i>
-                <i class="fab fa-twitter"></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-youtube"></i>
-            </li>
-        </ul>
-        <ul class="flex-col-1">
-            <li><img src="app/images/co-founder.jpg" alt="Co founder"></li>
-            <li><h6>Oliver Doel</h6></li>
-            <li>CEO & Founder</li>
-            <li class="flex-around">
-                <i class="fab fa-facebook"></i>
-                <i class="fab fa-twitter"></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-youtube"></i>
-            </li>
-        </ul>
-        <ul class="flex-col-1">
-            <li><img src="app/images/co-founder.jpg" alt="Co founder"></li>
-            <li><h6>Oliver Doel</h6></li>
-            <li>CEO & Founder</li>
-            <li class="flex-around">
-                <i class="fab fa-facebook"></i>
-                <i class="fab fa-twitter"></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-youtube"></i>
+                <a href="https://www.facebook.com/" target="_blank" title="www.facebook.com"><i class="fab fa-facebook"></i></a>
+                <a href="https://twitter.com/" target="_blank" title="www.twitter.com"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/" target="_blank" title="www.instagram.com"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.youtube.com/" target="_blank" title="www.youtube.com"><i class="fab fa-youtube"></i></a>
             </li>
         </ul>
     </div>
