a23a145d76dff27873c3178c58240bd2756515e6
grum1n <grumin.el.pastas@gmail.com>
2021-08-26 11:10:57 +0300

    added team-link style to team page

--- app/views/team.php
+++ app/views/team.php
@@ -4,3 +4,15 @@
         <p>Home / Team</p>
     </div>
 </section>
+<section class="white-container">
+    <div class="container team">
+        <div class="section-heading">
+            <h1>Join Our Team ?</h1>
+            <p class="text-small">Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="team-link">
+                <a href="" class="">LEARN MORE</a>
+                <a href="">JOIN US NOW</a>
+        </div>
+    </div>
+</section>
