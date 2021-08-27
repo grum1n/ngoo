c49b9c1729f2ad1cfa88172931e01fecab43adc5
grum1n <grumin.el.pastas@gmail.com>
2021-08-27 13:56:01 +0300

    added content in to news-grid-single-content

--- app/views/inc/news-grid-single-content.php
+++ app/views/inc/news-grid-single-content.php
@@ -1,7 +1,81 @@
 <section class="white-container">
     <div class="container news-single">
-        <div class>
-             test
+        <div class="col-1 flex-col-1">
+            <div class="news-single-about">
+                <h5>About Company</h5>
+                <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
+            </div>
+            <ul>
+                <li><i class="fas fa-bullhorn"></i></li>
+                <li><h6>Become a Volunteer</h6></li>
+                <li>We need you now for world</li>
+                <li>Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</li>
+                <li><button class="c2">JOIN US NOW</button></li>
+            </ul>
+            <div class="downl-brochure flex-container">
+                <div>
+                    <h6>Download Brochure</h6>   
+                    <p>Click here to download .PDF</p>
+                </div>
+                <a href=""><i class="fas fa-file-pdf"></i></a>
+            </div>
+            <div class="news-single-contact ">
+                <h5>Contact Info</h5>
+                <div class="events-single-info-row flex-container">
+                    <a href="https://goo.gl/maps/hnLKFDETpUnYTbQv8" target="_blank"><i class="events-single-info-icon fas fa-map"></i></a>  
+                    <span>Naujoji Akmenė</span> 
+                </div>
+                <div  class="events-single-info-row">
+                    <a href="mailto:info@yoursite.com"><i class="events-single-info-icon far fa-envelope"></i></a> 
+                    info@yoursite.com
+                </div>
+                <div  class="events-single-info-row">
+                    <a href="tel:+370600123456"><i class="events-single-info-icon fas fa-phone-alt"></i></a> 
+                    (+370) 600-123456
+                </div>
+            </div>
+        </div>
+        <div class="col-2 flex-col-1">
+            <img src="app/images/big_5.jpg" alt="Clock">
+            <h5>Etiam eleifend ac tellus aliquet congue.</h5>
+            <ul class="flex-container">
+                <li><i class="far fa-clock"></i> Apr 25, 2021</li>
+                <li>Posted by: Rome Doel</li>
+                <li>Category: Industry, Machine</li>
+                <li><i class="far fa-comment"></i> 0 Comments</li>
+            </ul>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div class="note">
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Carol Mongol</p>
+            </div>
+            <h3>Mollit anim idest laborum</h3>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <ol>
+                <li>Officia deserunt mollit anim id est laborum</li>
+                <li>Excepteur sintoccaecat cupidatat</li>
+                <li>Reprehenderit in voluptate velit</li>
+                <li>Exercitation ullamco laboris</li>
+            </ol>
+            <p>Utenim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sintoccaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h5><span class="text-orange">Leave a Reply</span></h5>
+            <p>Your email address will not be published. Required fields are marked *</p>
+            <form action="">
+                <label for="">Comment</label>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <label for="">Name *</label>
+                <input type="text">
+                <label for="">Email *</label>
+                <input type="text">
+                <label for="">Website</label>
+                <input type="text">
+                <div class="check flex-container">
+                    <input type="checkbox">
+                    <p>Save my name, email, and website in this browser for the next time I comment.</p>
+                </div>
+                <button class="c2">POST COMMENT</button>
+            </form>
         </div>
     </div>
 </section>
